e861d7c8c76ec6cb2423740c9a85c3b06991d5f1
root <sushi.honda@gmail.com>
2025-06-20 22:27:19 +0800

    ex01) feat: prototype

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 21:10:45 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/20 22:26:15 by athonda          ###   ########.fr       */
+/*   Created: 2025/06/20 22:13:56 by athonda           #+#    #+#             */
+/*   Updated: 2025/06/20 22:21:24 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 #include <iostream>
-#include <string>
-#include "Iter.hpp"
 
-int	main(void)
+template<typename T>
+void	iter(T *array, int length, void(*f)(T const &))
 {
-	std::string str[] = {"I", "am", "a", "42Singapore", "student"};
-	int	length = sizeof(str) / sizeof(str[0]);
-	iter(str, length, display);
-	return (0);
+	if (array == NULL || f == NULL)
+		return ;
+	for (int i = 0; i < length; ++i)
+	{
+		f(array[i]);
+	}
+}
+
+template<typename T>
+void	display(T const &s)
+{
+	std::cout << s <<std::endl;
 }
