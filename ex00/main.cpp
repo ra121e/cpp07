5804defbfe9c4cfb63610d1eafe4c094c58f1e7a
root <sushi.honda@gmail.com>
2025-06-22 10:29:55 +0800

    ex00) tests: Banner & more test cases

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 16:52:08 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/20 20:22:56 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/22 10:29:32 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 #include <iostream>
 #include "whatever.hpp"
 
+void	Banner(std::string title)
+{
+	std::cout << "\n--- " << title << " ---" << std::endl;
+}
 
 int	main(void)
 {
 	{
+	Banner("case: int");
 	int	a = 42;
 	int	b = 24;
 
@@ -31,6 +36,7 @@ int	main(void)
 	std::cout << "after swap: a " << a << std::endl;
 	}
 	{
+	Banner("case: float");
 	float	a = 42.3;
 	float	b = 24.6;
 
@@ -43,6 +49,20 @@ int	main(void)
 	swap(a, b);
 	std::cout << "after swap: a " << a << std::endl;
 
+	}
+	{
+	Banner("case: char");
+	char	a = 'A';
+	char	b = 'z';
+
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << b << std::endl;
+
+	std::cout << "max() of a or b: " << max(a, b) << std::endl;
+	std::cout << "min() of a or b: " << min(a, b) << std::endl;
+
+	swap(a, b);
+	std::cout << "after swap: a " << a << std::endl;
 	}
 	return (0);
 }
