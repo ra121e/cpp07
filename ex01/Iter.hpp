212a3e4ca0f845597af4a9902ad9c8a0a5aa5d53
root <sushi.honda@gmail.com>
2025-06-22 11:09:07 +0800

    ex01) tests: add() case

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 22:13:56 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/20 22:21:24 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/22 11:08:34 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <iostream>
 
 template<typename T>
-void	iter(T *array, int length, void(*f)(T const &))
+void	iter(T *array, int length, void(*f)(T const &s))
 {
 	if (array == NULL || f == NULL)
 		return ;
@@ -28,4 +28,11 @@ template<typename T>
 void	display(T const &s)
 {
 	std::cout << s <<std::endl;
+}
+
+// only for arithmatic T
+template <typename T>
+void	add(T const &s)
+{
+	std::cout << s + 5 << std::endl;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 21:10:45 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/20 22:26:15 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/22 11:08:14 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,37 @@
 #include <string>
 #include "Iter.hpp"
 
+void	Banner(std::string title)
+{
+	std::cout << "\n--- " << title << " ---" << std::endl;
+}
+
 int	main(void)
 {
+	{
+	Banner("case: std::string with display func()");
 	std::string str[] = {"I", "am", "a", "42Singapore", "student"};
 	int	length = sizeof(str) / sizeof(str[0]);
 	iter(str, length, display);
+	}
+	{
+//	Banner("case: std::string with add()");
+//	std::string str[] = {"I", "am", "a", "42Singapore", "student"};
+//	int	length = sizeof(str) / sizeof(str[0]);
+//	iter(str, length, add);
+	}
+	{
+	// test for compile error T is char
+//	Banner("case: char with add()");
+//	char str[] = {'A', 'B', 'C'};
+//	int	length = sizeof(str) / sizeof(str[0]);
+//	iter(str, length, add);
+	}
+	{
+	Banner("case: int with add()");
+	int str[] = {42, 24, 57, 111, 0};
+	int	length = sizeof(str) / sizeof(str[0]);
+	iter(str, length, add);
+	}
 	return (0);
 }
