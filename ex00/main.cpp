7961a82baa0c60556119bd3fdb8c13c4444f6e26
root <sushi.honda@gmail.com>
2025-06-24 15:39:09 +0800

    ex00) tests: add mandatory test case in main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 16:52:08 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/22 10:29:32 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/24 15:38:01 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,21 @@ int	main(void)
 
 	swap(a, b);
 	std::cout << "after swap: a " << a << std::endl;
+	}
+	{
+	Banner("Mandatory case");
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	}
 	return (0);
 }
